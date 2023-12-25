e132865a8e669419bf39ea3a6171876af29ed3ec
Gavazin Alexander <alexandergavazin@gmail.com>
2023-12-25 19:13:43 +0300

    add homework 4

--- terraform-04/src/providers.tf
+++ terraform-04/src/providers.tf
@@ -1,28 +1,15 @@
-# terraform {
-#   required_providers {
-#     yandex = {
-#       source = "yandex-cloud/yandex"
-#     }
-#   }
-#   required_version = ">=0.13"
-# }
-
 terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = ">=2.0.0" # Замените на более позднюю версию, поддерживающую вашу платформу
     }
   }
   required_version = ">=0.13"
 }
 
 provider "yandex" {
-  token     = var.yc_token
-  cloud_id  = var.yc_cloud_id
-  folder_id = var.yc_folder_id
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
   zone      = var.default_zone
 }
